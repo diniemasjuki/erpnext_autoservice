2654b0d3b72f1b93f90f9d0adbc490c37df2b49c
Dinie Masjuki <diniemasjuki@users.noreply.github.com>
2025-03-01 11:27:57 +0800

    Update job_sheet.js

--- autoservice/autoservice/doctype/job_sheet/job_sheet.js
+++ autoservice/autoservice/doctype/job_sheet/job_sheet.js
@@ -37,7 +37,17 @@ frappe.ui.form.on('Job Sheet', {
 
 
 
-
+frappe.ui.form.on('Job Sheet', {
+    refresh: function(frm) {
+        // Make the field editable if the value is 0
+        if (frm.doc.last_odometer == 0) {
+            frm.fields_dict['last_odometer'].df.read_only = 0;  // Make it editable
+        } else {
+            frm.fields_dict['last_odometer'].df.read_only = 1;  // Make it read-only
+        }        
+        frm.refresh_field('last_odometer'); // Refresh the field to apply the changes
+    }
+});
 
 frappe.ui.form.on('Job Sheet', {
     setup:function(frm){
