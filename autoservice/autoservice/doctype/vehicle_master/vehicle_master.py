4fbe8221a9c986571af4fe1e6ff1fbe89e895a4f
Dinie Masjuki <diniemasjuki@users.noreply.github.com>
2025-03-01 10:54:31 +0800

    Update vehicle_master.py

--- autoservice/autoservice/doctype/vehicle_master/vehicle_master.py
+++ autoservice/autoservice/doctype/vehicle_master/vehicle_master.py
@@ -9,12 +9,32 @@ from frappe.model.document import Document
 
 class VehicleMaster(Document):
 
+	def validate(self):		
+		if getdate(self.start_date) > getdate(self.end_date):
+			frappe.throw(_("Service Contract Start Date should be less than Service Contract End Date"))
+		if getdate(self.warranty_start_date) > getdate(self.warranty_end_date):
+			frappe.throw(_("Service Contract Start Date should be less than Service Contract End Date"))
+		
 	def before_insert(self):
 		if self.registration_no:
 			self.registration_no = self.registration_no.replace(" ", "").upper()
-
-	def before_save(self):
+		if self.engine_no:
+			self.engine_no = self.engine_no.replace(" ", "").upper()
 		if self.vin_number:
 			self.vin_number = self.vin_number.replace(" ", "").upper()
+		if self.chassis_no:
+			self.chassis_no = self.chassis_no.replace(" ", "").upper()
+		if self.model:
+			self.model = self.model.title()
+
+	def before_save(self):		
 		if self.registration_no:
 			self.registration_no = self.registration_no.replace(" ", "").upper()
+		if self.engine_no:
+			self.engine_no = self.engine_no.replace(" ", "").upper()
+		if self.vin_number:
+			self.vin_number = self.vin_number.replace(" ", "").upper()
+		if self.chassis_no:
+			self.chassis_no = self.chassis_no.replace(" ", "").upper()
+		if self.model:
+			self.model = self.model.title()
